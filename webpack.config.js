912a2b7947abe99a906a690e7f2781e1f645c36e
Sindy Stefani <sindystefani7@gmail.com>
2020-11-02 19:23:40 +0700

    fix bugs on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,25 @@ const path = require("path");
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-
-    entry:  './src/index.js',
-
+    entry: path.resolve(__dirname, "src/index.js"),
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'roov'
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+        library: "roov",
+        libraryTarget: "umd",
+        globalObject: `typeof self !== 'undefined' ? self : this`
     },
-
     module: {
         rules: [
             {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /(node_modules)/
-            }
-        ]
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
     },
-
+    mode: "production",
+    devtool: "source-map",
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
